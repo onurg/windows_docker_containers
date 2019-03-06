bc0650785bcebcf600b29ae0f2f73b86790d6c2e
onur.gokkocabas <onur.gokkocabas@globant.com>
2019-03-06 11:30:26 -0500

    Add ASP Net Web Forms app

---
+++ HelloWorld_ASP/HelloWorldASP/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace HelloWorldASP
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs args)
+        {
+            HelloWorldLabel.Text = "Hello, world!";
+        }
+    }
+}
